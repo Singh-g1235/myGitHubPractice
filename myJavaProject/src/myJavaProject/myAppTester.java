e7d407b590a18445c8dd52a45150c1ba90693186
Gurwinder Singh <singhg97@student.douglascollege.ca>
2020-05-21 19:42:22 -0700

    Gurwinder

---
+++ myJavaProject/src/myJavaProject/myAppTester.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package myJavaProject;
+
+/**
+ * @author GURWINDER
+ *
+ */
+public class myAppTester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hey my name is Gurwinder Singh");
+
+	}
+
+}
